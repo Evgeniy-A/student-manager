fdc45cffa8085715f5a2c628f11f78a5a846265d
rultsoviki <ryltsov.sergei@yandex.ru>
2025-11-23 13:44:54 +0300

    "часть рыльцова"

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
+import domain.Schedule;
+import repository.PasswordRepository;
+import repository.StudentScheduleRepository;
+import service.AuthorizationService;
+import service.RegistrationService;
+import service.ScheduleAndGradeService;
+import ui.ConsoleUI;
+
 public class Main {
+    public static void main(String[] args) {
+        PasswordRepository passwordRepository = new PasswordRepository();
+        StudentScheduleRepository studentScheduleRepository = new StudentScheduleRepository();
+        RegistrationService registrationService = new RegistrationService(passwordRepository, studentScheduleRepository);
+        Schedule schedule = new Schedule();
+        AuthorizationService authorizationService = new AuthorizationService(passwordRepository);
+        ScheduleAndGradeService scheduleAndGradeService = new ScheduleAndGradeService(studentScheduleRepository, schedule);
+        ConsoleUI consoleUI = new ConsoleUI(registrationService, authorizationService, scheduleAndGradeService);
+        consoleUI.start();
+    }
 }
---
+++ src/service/AuthorizationService.java
@@ -0,0 +1,44 @@
+package service;
+
+import domain.Student;
+import domain.Teacher;
+import repository.PasswordRepository;
+
+public class AuthorizationService {
+    private final PasswordRepository passwordRepository;
+
+    public AuthorizationService(PasswordRepository passwordRepository) {
+        this.passwordRepository = passwordRepository;
+    }
+
+
+    public boolean authorizationStudent(String login, String password) {
+        Student student1 = passwordRepository.getStudent(login);
+        if (student1.getPasswordHash().equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Student getStud(String name) {
+        return passwordRepository.getStudent(name);
+    }
+
+    public boolean authorizationTeacher(String login, String password) {
+        Teacher teacher1 = passwordRepository.getTeacher(login);
+        if (teacher1.getPasswordHash().equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Teacher getTeacher(String login) {
+        return passwordRepository.getTeacher(login);
+    }
+
+    public Student getStudName(String name) {
+        return passwordRepository.getNameStudent(name);
+    }
+}
+
+
